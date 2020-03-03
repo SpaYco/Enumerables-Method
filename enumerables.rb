af51abf948f0ceb91b6ffb69707829ea1b859ea6
SpaYco <zizou.aziz101@gmail.com>
2020-03-03 15:23:01 +0100

    added my_each_with_index method

--- enumerables.rb
+++ enumerables.rb
@@ -4,8 +4,18 @@ module Enumerable
             yield(arr[v])
         end
     end
+    def my_each_with_index(arr)
+        (arr.length).times do |v|
+            yield(arr[v], v)
+        end
+    end
 end
 include Enumerable
 my_each(["hi", "bye", "lol"]) do |k|
     puts k
+end
+
+my_each_with_index(["hi", "bye", "lol"]) do |x, y|
+    puts x
+    puts y
 end
