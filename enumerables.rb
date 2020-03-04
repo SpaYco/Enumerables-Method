73813df07e1ab45215ebeb9a56b31737507803b3
SpaYco <zizou.aziz101@gmail.com>
2020-03-04 10:20:07 +0100

    added my_inject method

--- enumerables.rb
+++ enumerables.rb
@@ -27,6 +27,7 @@ module Enumerable
     end
     result
   end
+
   def my_any?
     result = false
     length.times do |v|
@@ -35,6 +36,7 @@ module Enumerable
     end
     result
   end
+
   def my_none?
     result = true
     length.times do |v|
@@ -43,20 +45,30 @@ module Enumerable
     end
     result
   end
+
   def my_count
     result = 0
     length.times do |v|
-      if yield(self[v])
-        result += 1 
-      end
+      result += 1 if yield(self[v])
     end
     result
   end
+
   def my_map
     result = []
     length.times do |v|
       yiel_var = yield(self[v])
       result.push(yiel_var)
+    end
+    result
+  end
+
+  def my_inject
+    self_arr = to_a
+    result = self_arr[0]
+    (self_arr.length - 1).times do |v|
+      yiel_var = yield(result, self_arr[v + 1])
+      result = yiel_var
     end
     result
   end
